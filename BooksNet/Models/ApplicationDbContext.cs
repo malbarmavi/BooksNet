ec4c4ff42e0bab5e2b501aba9509aadcae546b8f
mbarmawi <muhammadalbarmawi@outlook.com>
2017-03-08 07:34:40 +0300

    Update EF DbContext

---
+++ BooksNet/Models/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace BooksNet.Models
+{
+  public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+  {
+    public ApplicationDbContext()
+        : base("BooksNetDataBae", throwIfV1Schema: false)
+    {
+    }
+
+    public static ApplicationDbContext Create()
+    {
+      return new ApplicationDbContext();
+    }
+  }
+}
