bbbfbcd70c420990ef873b0a64195eed48c4937b
mbarmawi <muhammadalbarmawi@outlook.com>
2017-03-12 18:55:47 +0300

    Add timestamp migration

---
+++ BooksNet/Migrations/201703121554014_AddTimestampToModels.cs
@@ -0,0 +1,24 @@
+namespace BooksNet.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddTimestampToModels : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Authors", "Version", c => c.Binary(nullable: false, fixedLength: true, timestamp: true, storeType: "rowversion"));
+            AddColumn("dbo.Books", "Version", c => c.Binary(nullable: false, fixedLength: true, timestamp: true, storeType: "rowversion"));
+            AddColumn("dbo.Categories", "Version", c => c.Binary(nullable: false, fixedLength: true, timestamp: true, storeType: "rowversion"));
+            AddColumn("dbo.Publishers", "Version", c => c.Binary(nullable: false, fixedLength: true, timestamp: true, storeType: "rowversion"));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Publishers", "Version");
+            DropColumn("dbo.Categories", "Version");
+            DropColumn("dbo.Books", "Version");
+            DropColumn("dbo.Authors", "Version");
+        }
+    }
+}
