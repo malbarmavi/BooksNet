9b89693d50dd2fc2e03a248eb54b9410eaa17599
mbarmawi <muhammadalbarmawi@outlook.com>
2017-03-14 01:07:07 +0300

    Add migration update models

---
+++ BooksNet/Migrations/201703132036338_UpdateModels.cs
@@ -0,0 +1,34 @@
+namespace BooksNet.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class UpdateModels : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Authors", "Pages", c => c.Int(nullable: false));
+            AddColumn("dbo.Authors", "CreateDate", c => c.DateTime(nullable: false));
+            AddColumn("dbo.Authors", "LastUpdate", c => c.DateTime(nullable: false));
+            AddColumn("dbo.Books", "CreateDate", c => c.DateTime(nullable: false));
+            AddColumn("dbo.Books", "LastUpdate", c => c.DateTime(nullable: false));
+            AddColumn("dbo.Categories", "CreateDate", c => c.DateTime(nullable: false));
+            AddColumn("dbo.Categories", "LastUpdate", c => c.DateTime(nullable: false));
+            AddColumn("dbo.Publishers", "CreateDate", c => c.DateTime(nullable: false));
+            AddColumn("dbo.Publishers", "LastUpdate", c => c.DateTime(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Publishers", "LastUpdate");
+            DropColumn("dbo.Publishers", "CreateDate");
+            DropColumn("dbo.Categories", "LastUpdate");
+            DropColumn("dbo.Categories", "CreateDate");
+            DropColumn("dbo.Books", "LastUpdate");
+            DropColumn("dbo.Books", "CreateDate");
+            DropColumn("dbo.Authors", "LastUpdate");
+            DropColumn("dbo.Authors", "CreateDate");
+            DropColumn("dbo.Authors", "Pages");
+        }
+    }
+}
