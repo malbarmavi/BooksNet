eeb58fde047f76bb3833f03fe9a192e9d9354af1
mbarmawi <muhammadalbarmawi@outlook.com>
2017-03-15 23:39:57 +0300

    Add dashboard controller

---
+++ BooksNet/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace BooksNet.Areas.Admin.Controllers
+{
+  public class DashboardController : Controller
+  {
+    [Authorize]
+    public ActionResult Index()
+    {
+      return View();
+    }
+  }
+}
