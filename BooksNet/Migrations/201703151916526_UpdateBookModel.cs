f6204422e39770761414ab92a82f659245ccd142
mbarmawi <muhammadalbarmawi@outlook.com>
2017-03-15 22:21:25 +0300

    Add book model update migration

---
+++ BooksNet/Migrations/201703151916526_UpdateBookModel.cs
@@ -0,0 +1,18 @@
+namespace BooksNet.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class UpdateBookModel : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Books", "PagesNumber", c => c.Int(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Books", "PagesNumber");
+        }
+    }
+}
