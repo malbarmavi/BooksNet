20353731955b87ff77ca7bfeefb809856487e6bd
Muhammad Albarmawi <MuhammadAlbarmawi@outlook.com>
2017-03-19 00:42:24 +0300

    Add roles class

---
+++ BooksNet/Areas/Admin/Models/Roles.cs
@@ -0,0 +1,11 @@
+﻿namespace BooksNet.Areas.Admin.Models
+{
+  public static class Roles
+  {
+    public static string Admin { get; set; } = nameof(Admin);
+
+    public static string SubAdmin { get; set; } = nameof(SubAdmin);
+
+    public static string Staff { get; set; } = nameof(Staff);
+  }
+}
