3ed6551caf4bccce3cc31e9a5d8216a50dbc3d8f
Muhammad Albarmawi <MuhammadAlbarmawi@outlook.com>
2017-03-19 09:17:23 +0300

    Initialize Identity for EF

--- BooksNet/Migrations/Configuration.cs
+++ BooksNet/Migrations/Configuration.cs
@@ -1,6 +1,11 @@
 namespace BooksNet.Migrations
 {
+  using BooksNet.Areas.Admin.Models;
   using BooksNet.Models;
+  using Microsoft.AspNet.Identity;
+  using Microsoft.AspNet.Identity.EntityFramework;
+  using System;
+  using System.Linq;
   using System.Data.Entity.Migrations;
 
   internal sealed class Configuration : DbMigrationsConfiguration<ApplicationDbContext>
@@ -12,6 +17,41 @@ namespace BooksNet.Migrations
 
     protected override void Seed(ApplicationDbContext context)
     {
+      // create roles
+      var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+
+      string[] roles = new string[] { Roles.Admin, Roles.SubAdmin, Roles.Staff };
+
+      foreach (string role in roles)
+      {
+        if (!roleManager.RoleExists(role))
+        {
+          roleManager.Create(new IdentityRole(role));
+        }
+      }
+
+      // create default admin user
+
+      if (!context.Users.Any())
+      {
+
+        ApplicationUser admin = new ApplicationUser()
+        {
+          Email = "admin@booksnet.com",
+          UserName = "admin@booksnet.com",
+          FirstName = "admin",
+          LastName = "admin",
+          CreateDate = DateTime.Now,
+          LastUpdate = DateTime.Now
+        };
+
+        var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+        var result = userManager.Create(admin, "Asd!23");
+        if (result.Succeeded)
+        {
+          userManager.AddToRole(admin.Id, Roles.Admin);
+        }
+      }
     }
   }
 }
