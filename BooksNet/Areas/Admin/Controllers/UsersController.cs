1b54efd65dffd09846e77201491cadd095ffa119
Muhammad Albarmawi <MuhammadAlbarmawi@outlook.com>
2017-03-22 02:46:24 +0300

    Update user view layout

--- BooksNet/Areas/Admin/Controllers/UsersController.cs
+++ BooksNet/Areas/Admin/Controllers/UsersController.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 
 namespace BooksNet.Areas.Admin.Controllers
 {
+  [Authorize]
   public class UsersController : Controller
   {
     private ApplicationDbContext db = new ApplicationDbContext();
