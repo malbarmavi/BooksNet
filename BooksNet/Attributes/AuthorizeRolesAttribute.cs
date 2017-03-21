4b585e964532dffed37a7cc8b84b6969d97d28d5
Muhammad Albarmawi <MuhammadAlbarmawi@outlook.com>
2017-03-22 02:46:05 +0300

    Extend AuthorizeAttribute

---
+++ BooksNet/Attributes/AuthorizeRolesAttribute.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BooksNet.Attributes
+{
+  public class AuthorizeRolesAttribute : AuthorizeAttribute
+  {
+    public AuthorizeRolesAttribute(params string[] roles) : base()
+    {
+      Roles = string.Join(",", roles);
+    }
+
+    protected override void HandleUnauthorizedRequest(AuthorizationContext filterContext)
+    {
+      if (filterContext.HttpContext.User.Identity.IsAuthenticated)
+      {
+        filterContext.Result = new RedirectResult("/Admin/Error/AccessDenied");
+      }
+      else
+      {
+        base.HandleUnauthorizedRequest(filterContext);
+      }
+    }
+  }
+}
