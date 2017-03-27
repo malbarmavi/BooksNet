57d801d696c00e017fc18042f195c46a685bb1a5
Muhammad Albarmawi <MuhammadAlbarmawi@outlook.com>
2017-03-27 14:34:34 +0300

    Add gulp plugins

--- BooksNet/gulpfile.js
+++ BooksNet/gulpfile.js
@@ -1,4 +1,9 @@
-﻿var gulp = require('gulp');
+﻿'use strict'
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var cssnano = require('gulp-cssnano');
+var concat = require('gulp-concat'); 
 
 gulp.task('default', function () {
    
