ef41ffeff2e41fbd242124e07403aeb9d67db316
Muhammad Albarmawi <MuhammadAlbarmawi@outlook.com>
2017-03-30 09:23:33 +0300

    Add build number to css layout file

---
+++ BooksNet/AppSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+
+namespace BooksNet
+{
+  public static class AppSettings
+  {
+    // get build number
+    public static string Build { get; } = Assembly.GetExecutingAssembly().GetName().Version.Build.ToString();
+  }
+}
