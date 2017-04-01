0633e840ce2fe89a6fd815d70367c63df7985aba
Muhammad Albarmawi <MuhammadAlbarmawi@outlook.com>
2017-04-01 06:55:44 +0300

    Apply dashboard prototype

--- BooksNet/Areas/Admin/Controllers/DashboardController.cs
+++ BooksNet/Areas/Admin/Controllers/DashboardController.cs
@@ -1,13 +1,30 @@
-﻿using System.Web.Mvc;
+﻿using BooksNet.Areas.Admin.ViewModels.Dashboard;
+using BooksNet.Models;
+using System.Web.Mvc;
+using System.Linq;
 
 namespace BooksNet.Areas.Admin.Controllers
 {
   public class DashboardController : Controller
   {
+    private ApplicationDbContext db = new ApplicationDbContext();
+
     [Authorize]
     public ActionResult Index()
     {
-      return View();
+      DashboardViewModel model = new DashboardViewModel();
+      model.UsersCount = db.Users.Count();
+      return View(model);
     }
+
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing)
+      {
+        db.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
   }
 }
---
+++ BooksNet/Areas/Admin/ViewModels/Dashboard/DashboardViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BooksNet.Areas.Admin.ViewModels.Dashboard
+{
+  public class DashboardViewModel
+  {
+    public int UsersCount { get; set; }
+
+    public int AuthorsCount { get; set; }
+
+    public int PublishersCount { get; set; }
+
+    public int BooksCount { get; set; }
+  }
+}
