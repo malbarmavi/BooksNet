fdd7dae3d2126b24bfc80f7d0bc11f1d2abdc2db
Muhammad Albarmawi <MuhammadAlbarmawi@outlook.com>
2017-04-02 12:37:34 +0300

    Add publishers controller

---
+++ BooksNet/Areas/Admin/Controllers/PublishersController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BooksNet.Models;
+
+namespace BooksNet.Areas.Admin.Controllers
+{
+    public class PublishersController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Admin/Publishers
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.Publishers.ToListAsync());
+        }
+
+        // GET: Admin/Publishers/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Publisher publisher = await db.Publishers.FindAsync(id);
+            if (publisher == null)
+            {
+                return HttpNotFound();
+            }
+            return View(publisher);
+        }
+
+        // GET: Admin/Publishers/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Publishers/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Id,Name,Address,Version,CreateDate,LastUpdate")] Publisher publisher)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Publishers.Add(publisher);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(publisher);
+        }
+
+        // GET: Admin/Publishers/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Publisher publisher = await db.Publishers.FindAsync(id);
+            if (publisher == null)
+            {
+                return HttpNotFound();
+            }
+            return View(publisher);
+        }
+
+        // POST: Admin/Publishers/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,Name,Address,Version,CreateDate,LastUpdate")] Publisher publisher)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(publisher).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(publisher);
+        }
+
+        // GET: Admin/Publishers/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Publisher publisher = await db.Publishers.FindAsync(id);
+            if (publisher == null)
+            {
+                return HttpNotFound();
+            }
+            return View(publisher);
+        }
+
+        // POST: Admin/Publishers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Publisher publisher = await db.Publishers.FindAsync(id);
+            db.Publishers.Remove(publisher);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
