51e6dbe52ab0b8dba0e3cc2aa9dad39712c5ea56
Muhammad Albarmawi <MuhammadAlbarmawi@outlook.com>
2017-04-19 15:30:31 +0300

    Update web api area

---
+++ BooksNet/Areas/Api/Controllers/BooksController.cs
@@ -0,0 +1,44 @@
+﻿using BooksNet.Models;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace BooksNet.Areas.Api.Controllers
+{
+  public class BooksController : ApiController
+  {
+    private ApplicationDbContext db = new ApplicationDbContext();
+
+    public IQueryable<Book> GetBooks()
+    {
+      return db.Books;
+    }
+
+    [ResponseType(typeof(Book))]
+    public async Task<IHttpActionResult> GetBook(int id)
+    {
+      Book book = await db.Books.FindAsync(id);
+      if (book == null)
+      {
+        return NotFound();
+      }
+
+      return Ok(book);
+    }
+
+    private bool BookExists(int id)
+    {
+      return db.Books.Count(e => e.Id == id) > 0;
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing)
+      {
+        db.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+  }
+}
--- BooksNet/Global.asax.cs
+++ BooksNet/Global.asax.cs
@@ -1,4 +1,6 @@
-﻿using System.Web.Mvc;
+﻿using BooksNet.Areas.Api;
+using System.Web.Http;
+using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 
@@ -9,6 +11,7 @@ namespace BooksNet
     protected void Application_Start()
     {
       AreaRegistration.RegisterAllAreas();
+      GlobalConfiguration.Configure(WebApiConfig.Register);
       FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
       RouteConfig.RegisterRoutes(RouteTable.Routes);
       BundleConfig.RegisterBundles(BundleTable.Bundles);
