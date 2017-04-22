e1075cb4882650aec3c6708f8a23df63e70dff8c
Muhammad Albarmawi <MuhammadAlbarmawi@outlook.com>
2017-04-22 18:09:35 +0300

    Add top views service

---
+++ BooksNet/Areas/Api/Controllers/TopViewsController.cs
@@ -0,0 +1,25 @@
+﻿using BooksNet.Models;
+using System.Linq;
+using System.Web.Http;
+
+namespace BooksNet.Areas.Api.Controllers
+{
+  public class TopViewsController : ApiController
+  {
+    private ApplicationDbContext db = new ApplicationDbContext();
+
+    public IQueryable<Book> Get()
+    {
+      return db.Books.OrderByDescending(b => b.Views).Take(20);
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing)
+      {
+        db.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+  }
+}
