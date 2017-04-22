df8ee194b1d4f029de89ab19468596928f86f098
Muhammad Albarmawi <MuhammadAlbarmawi@outlook.com>
2017-04-22 18:03:28 +0300

    Add latest upload service

---
+++ BooksNet/Areas/Api/Controllers/LatestUploadController.cs
@@ -0,0 +1,25 @@
+﻿using BooksNet.Models;
+using System.Linq;
+using System.Web.Http;
+
+namespace BooksNet.Areas.Api.Controllers
+{
+  public class LatestUploadController : ApiController
+  {
+    private ApplicationDbContext db = new ApplicationDbContext();
+
+    public IQueryable<Book> GetLatestUpload()
+    {
+      return db.Books.OrderByDescending(b => b.Id).Take(20);
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing)
+      {
+        db.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+  }
+}
