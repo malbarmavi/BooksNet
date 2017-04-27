37a8f4f68f91fc0e25d01b1550c15697c5e8094c
Muhammad Albarmawi <MuhammadAlbarmawi@outlook.com>
2017-04-28 00:20:00 +0300

    Add attribute to restrict ajax request to actions

---
+++ BooksNet/Attributes/AjaxOnlyAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Web.Mvc;
+
+namespace BooksNet.Attributes
+{
+  public class AjaxOnly : ActionMethodSelectorAttribute
+  {
+    public override bool IsValidForRequest(ControllerContext controllerContext, MethodInfo methodInfo)
+    {
+      return controllerContext.HttpContext.Request.IsAjaxRequest();
+    }
+  }
+}
