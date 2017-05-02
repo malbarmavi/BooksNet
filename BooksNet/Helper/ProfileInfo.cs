666e3a85a56586b36d0d6c9921ad1dd2412c02b4
Muhammad Albarmawi <MuhammadAlbarmawi@outlook.com>
2017-05-02 16:08:13 +0300

    Handle null references ex

--- BooksNet/Helper/ProfileInfo.cs
+++ BooksNet/Helper/ProfileInfo.cs
@@ -7,10 +7,16 @@ namespace BooksNet.Helper
   {
     private static Profile _profile { get; set; } = (new ApplicationDbContext()).Profile.FirstOrDefault();
 
-    public static string Name { get; } = _profile.Name;
+    public static string Name { get; } = _profile.Name ?? Refrech().Name;
 
-    public static string Descraption { get; } = _profile.Description;
+    public static string Descraption { get; } = _profile.Description ?? Refrech().Description;
 
-    public static string Title { get; } = _profile.Title;
+    public static string Title { get; } = _profile.Title ?? Refrech().Title;
+
+    private static Profile Refrech()
+    {
+     _profile = (new ApplicationDbContext()).Profile.FirstOrDefault();
+      return _profile;
+    }
   }
 }
