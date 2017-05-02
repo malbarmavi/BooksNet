73f83c440f4a40cc155b6a5ca9cf31010ed6661b
Muhammad Albarmawi <MuhammadAlbarmawi@outlook.com>
2017-05-02 13:33:33 +0300

    Add profileinfo helper

---
+++ BooksNet/Helper/ProfileInfo.cs
@@ -0,0 +1,16 @@
+﻿using BooksNet.Models;
+using System.Linq;
+
+namespace BooksNet.Helper
+{
+  public static class ProfileInfo
+  {
+    private static Profile _profile { get; set; } = (new ApplicationDbContext()).Profile.FirstOrDefault();
+
+    public static string Name { get; } = _profile.Name;
+
+    public static string Descraption { get; } = _profile.Description;
+
+    public static string Title { get; } = _profile.Title;
+  }
+}
