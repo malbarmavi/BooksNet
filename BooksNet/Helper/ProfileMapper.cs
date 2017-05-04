4a8398e50400b19b65f9384cbefc8b1bbdcedbb1
Muhammad Albarmawi <MuhammadAlbarmawi@outlook.com>
2017-05-04 14:53:59 +0300

    Update ProfileMapper

---
+++ BooksNet/Helper/ProfileMapper.cs
@@ -0,0 +1,17 @@
+﻿using BooksNet.Models;
+using System.Linq;
+
+namespace BooksNet.Helper
+{
+  public static class ProfileMapper
+  {
+    private static Profile _profile { get; set; } = (new ApplicationDbContext()).Profile.FirstOrDefault();
+
+    public static Profile Get()
+    {
+      if (_profile == null)
+     _profile = (new ApplicationDbContext()).Profile.FirstOrDefault();
+      return _profile;
+    }
+  }
+}
