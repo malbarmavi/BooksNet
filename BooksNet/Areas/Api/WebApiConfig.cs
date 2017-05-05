bf3754077b75a5cb7bf2d1d7ca098dc2eac79155
Muhammad Albarmawi <MuhammadAlbarmawi@outlook.com>
2017-05-05 16:46:48 +0300

    Enable gzip compression for web api

--- BooksNet/Areas/Api/WebApiConfig.cs
+++ BooksNet/Areas/Api/WebApiConfig.cs
@@ -1,4 +1,6 @@
-﻿using System.Net.Http.Formatting;
+﻿using Microsoft.AspNet.WebApi.Extensions.Compression.Server;
+using System.Net.Http.Extensions.Compression.Core.Compressors;
+using System.Net.Http.Formatting;
 using System.Web.Http;
 
 namespace BooksNet.Areas.Api
@@ -9,6 +11,10 @@ namespace BooksNet.Areas.Api
     {
       config.Formatters.Clear();
       config.Formatters.Add(new JsonMediaTypeFormatter());
+
+      config.MessageHandlers.Insert(0, 
+        new ServerCompressionHandler(new GZipCompressor(), new DeflateCompressor()));
+
       config.MapHttpAttributeRoutes();
       config.Routes.MapHttpRoute(
           name: "DefaultApi",
