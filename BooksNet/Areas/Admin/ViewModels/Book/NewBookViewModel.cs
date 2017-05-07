411bd03cd08f330ffcf70eb7985e123dd6115fb2
Muhammad Albarmawi <MuhammadAlbarmawi@outlook.com>
2017-05-07 16:57:22 +0300

    Implement globalization on books viewmodel , some pages and layout

--- BooksNet/Areas/Admin/ViewModels/Book/NewBookViewModel.cs
+++ BooksNet/Areas/Admin/ViewModels/Book/NewBookViewModel.cs
@@ -8,49 +8,58 @@ namespace BooksNet.Areas.Admin.ViewModels.Book
   public class NewBookViewModel
   {
     [Required]
+    [Display(Name = nameof(Title), ResourceType = typeof(GlobalResources.Resource))]
     public string Title { get; set; }
 
     [Required]
+    [Display(Name = nameof(Age), ResourceType = typeof(GlobalResources.Resource))]
     public AgeSet Age { get; set; }
 
- 
+    [Display(Name = nameof(Category), ResourceType = typeof(GlobalResources.Resource))]
     public SelectList Category { get; set; }
 
- 
     public int CategoryId { get; set; }
 
-   
+    [Display(Name = nameof(Categories), ResourceType = typeof(GlobalResources.Resource))]
     public MultiSelectList Categories { get; set; }
 
     [Required]
     public int[] CategoriesId { get; set; }
 
+    [Display(Name = nameof(Print), ResourceType = typeof(GlobalResources.Resource))]
     public string Print { get; set; }
 
+    [Display(Name = nameof(PrintDate), ResourceType = typeof(GlobalResources.Resource))]
     public string PrintDate { get; set; }
 
     [MaxLength(250)]
+    [Display(Name = nameof(Notes), ResourceType = typeof(GlobalResources.Resource))]
     public string Notes { get; set; }
 
-   
+    [Display(Name = nameof(Authors), ResourceType = typeof(GlobalResources.Resource))]
     public MultiSelectList Authors { get; set; }
 
     [Required]
     public int[] AuthorsId { get; set; }
 
+    [Display(Name = nameof(Publisher), ResourceType = typeof(GlobalResources.Resource))]
     public SelectList Publisher { get; set; }
 
     [Required]
     public int PublisherId { get; set; }
 
     [Required]
+    [Display(Name = nameof(File), ResourceType = typeof(GlobalResources.Resource))]
     public HttpPostedFileBase File { get; set; }
 
+    [Display(Name = nameof(CoverImage), ResourceType = typeof(GlobalResources.Resource))]
     public HttpPostedFileBase CoverImage { get; set; }
 
+    [Display(Name = nameof(PagesNumber), ResourceType = typeof(GlobalResources.Resource))]
     public int PagesNumber { get; set; }
 
     [MaxLength(500)]
+    [Display(Name = nameof(Descriptions), ResourceType = typeof(GlobalResources.Resource))]
     public string Descriptions { get; set; }
   }
 }
--- BooksNet/Models/AgeSet.cs
+++ BooksNet/Models/AgeSet.cs
@@ -1,7 +1,10 @@
-﻿namespace BooksNet.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BooksNet.Models
 {
   public enum AgeSet : int
   {
+    [Display(Name = "AllAges", ResourceType = typeof(GlobalResources.Resource))]
     All = 0,
     Adults = 1,
     Kids = 2
