45ec7a929ad203490455e7a8075a8286ae72512d
Muhammad Albarmawi <MuhammadAlbarmawi@outlook.com>
2017-05-24 11:59:06 +0300

    Update logoff redirect url

--- BooksNet/Areas/Admin/Controllers/AccountController.cs
+++ BooksNet/Areas/Admin/Controllers/AccountController.cs
@@ -397,7 +397,7 @@ namespace BooksNet.Areas.Admin.Controllers
     public ActionResult LogOff()
     {
       AuthenticationManager.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
-      return RedirectToAction("Index", "Home");
+      return RedirectToAction("Login", "Account");
     }
 
     //
